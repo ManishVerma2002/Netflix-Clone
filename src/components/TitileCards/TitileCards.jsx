edf7d9d697d414029c66e6424eaed7e25f0f90c5
Manish Verma <163527895+ManishVerma2002@users.noreply.github.com>
2025-02-27 17:38:34 +0530

    complete navbar and titleCard

---
+++ src/components/TitileCards/TitileCards.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from "react";
+import "./TitileCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+
+const TitileCards = ({title, category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h1> {title?title:"Popular on Netflix"}</h1>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.title} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitileCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitileCards from "../../components/TitileCards/TitileCards";
+import Footer from "../../components/Footer/Footer";
+
 const Home = () => {
   return (
-    <div className='home'></div>
-  )
-}
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="hero" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="hero" className="caption-img" />
+          <p>
+            Movies move us like nothing else can, whether they’re scary, funny,
+            dramatic, romantic or anywhere in-between. So many titles, so much
+            to experience.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              {" "}
+              <img src={play_icon} alt=""></img>Play
+            </button>
+            <button className="btn dark-btn">
+              {" "}
+              <img src={info_icon} alt=""></img>More Info
+            </button>
+          </div>
+          <TitileCards/>
+        </div>
+      </div>
+      <div className="more-cards">
+      <TitileCards title={"Blockbuster Movies"}/>
+      <TitileCards title={"Only on Netflix"}/>
+      <TitileCards title={"Upcoming"}/>
+      <TitileCards title={"Top Pics for you "}/>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
 
 export default Home;
